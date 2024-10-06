6c93e7598dc8189519f01223b2f3d1eae58bb417
sanjana-arjun <152984215+sanjana-arjun@users.noreply.github.com>
2024-10-06 17:01:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
+// index.js
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
-
 import App from './App'
+import register from './serviceWorkerRegistration' // Correct import
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 )
+
+register() // Call the register function
